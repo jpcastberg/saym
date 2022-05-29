976324d5677e4367cae1fa264b6fd8c66d309053
jpcastberg <jcastberg@extole.com>
2022-05-29 13:30:42 -0700

    file structure

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect, beforeEach, afterEach } from "@jest/globals";
 import supertest from "supertest";
-import app from "../src/server/app";
-import { dbConnect, dbClose } from "../src/server/database";
+import app from "../server/app";
+import { dbConnect, dbClose } from "../server/database";
 
 beforeEach(async () => {
     return await dbConnect();
