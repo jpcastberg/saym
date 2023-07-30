032a9639189374ea762e25f7f9c59403b9ab4582
John Castberg <jpcastberg@gmail.com>
2023-07-30 11:21:49 -0700

    Update client to latest vue

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,10 +4,13 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
     "root": true,
     "extends": [
-        "plugin:vue/vue3-recommended",
+        "plugin:vue/vue3-essential",
         "eslint:recommended",
-        "@vue/eslint-config-typescript/recommended",
+        "@vue/eslint-config-typescript"
     ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
     "rules": {
         "quotes": "error",
         "semi": "error",
@@ -15,12 +18,9 @@ module.exports = {
         "indent": [
             "error", 4
         ],
-        "vue/html-indent": [
-            "error", 4
-        ],
         "vue/script-indent": [
             "error", 4
         ],
-        "vue/html-self-closing": false
+        "vue/html-self-closing": 1
     }
 };
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,21 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    "plugins": [vue(), vueJsx()],
+    "plugins": [
+        vue(),
+        vueJsx(),
+        vuetify({ "autoImport": true }),
+    ],
     "server": {
-        "port": 3001,
         "proxy": {
             "^/api/.*": "http://localhost:3000"
         }
-    },
-    "build": {
-        "outDir": "../server/public",
-        "emptyOutDir": true
     },
     "resolve": {
         "alias": {
