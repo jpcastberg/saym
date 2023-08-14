9ca5fbcf3ebe56a90f0f61083627366a684c2abc
John Castberg <jpcastberg@gmail.com>
2023-08-13 22:06:24 -0700

    update models to new patterns

--- shared/models/GameModels.ts
+++ shared/models/GameModels.ts
@@ -1,20 +1,44 @@
-export interface GameModel {
+import { type UserModel } from "./UserModels";
+
+export interface GameDbModel {
     _id: string;
     playerOneUserId: string;
     playerTwoUserId: string | null;
     playerOneTurns: string[];
     playerTwoTurns: string[];
     isGameComplete: boolean;
+    needToInvitePlayer: boolean;
     lastUpdate: string;
 }
 
-export interface GameRequestModel {
+export interface GameResponseModel {
+    _id: string;
+    playerOne: UserModel | null;
+    playerTwo: UserModel | null;
+    playerOneTurns: string[];
+    playerTwoTurns: string[];
+    isGameComplete: boolean;
+    needToInvitePlayer: boolean;
+    lastUpdate: string;
+}
+
+export interface AllGamesResponseModel {
+    currentGames: GameResponseModel[];
+    finishedGames: GameResponseModel[];
+}
+
+export interface GameUpdateModel {
     playerOneUserId?: string;
     playerTwoUserId?: string | null;
     playerOneTurns?: string[];
     playerTwoTurns?: string[];
 }
 
-export interface TurnRequestModel {
+export interface TurnCreateModel {
     turn: string;
+}
+
+export interface GameWebsocketUpdateModel {
+    eventType: "gameUpdate";
+    data: GameResponseModel;
 }
