771def6cdc83a9914bf228e682aadacce240a0b9
John Castberg <jpcastberg@gmail.com>
2023-08-13 22:16:10 -0700

    refactor appStore

--- client/src/stores/app.ts
+++ client/src/stores/app.ts
@@ -4,19 +4,13 @@ import { defineStore } from "pinia";
 interface AppState {
     isAppDrawerOpen: boolean;
     appBarTitle: string;
-    usernameInput: string;
-    turnInput: string;
     shouldShowInvitePlayerDialog: boolean;
-    invitePlayerDialogGameId: string;
 }
 
 export const useAppStore = defineStore("app", {
     state: (): AppState => ({
         isAppDrawerOpen: false,
         appBarTitle: "",
-        usernameInput: "",
-        turnInput: "",
         shouldShowInvitePlayerDialog: false,
-        invitePlayerDialogGameId: "",
     }),
 });
