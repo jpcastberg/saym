79d10a846b70f8f97851506fe2d4c1cec14fa258
John Castberg <jpcastberg@gmail.com>
2023-08-24 13:33:30 -0700

    Rename UsernameModal -> UsernameDialog

---
+++ client/src/components/UsernameDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useUserStore } from "../stores/user";
+const userStore = useUserStore();
+const usernameInput = ref("");
+
+async function handleUsernameFormSubmit() {
+    await userStore.updateUsername(usernameInput.value);
+}
+</script>
+
+<template>
+    <v-dialog v-model="userStore.needsUsername">
+        <v-card>
+            <v-card-title class="px-5 pt-5 pb-0 d-flex justify-center">
+                <span class="text-h5">Welcome to Saym!</span>
+            </v-card-title>
+            <v-form class="pa-5 d-flex flex-column align-center" @submit.prevent="handleUsernameFormSubmit">
+                <v-card-text class="w-100">
+                    <v-text-field v-model="usernameInput" label="What should we call you?" required />
+                </v-card-text>
+                <v-card-actions class="py-0">
+                    <v-btn size="large" type="submit" color="teal-darken-1" variant="elevated">
+                        Let's Go!
+                    </v-btn>
+                </v-card-actions>
+            </v-form>
+        </v-card>
+    </v-dialog>
+</template>
