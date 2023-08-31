8fd22e4836a510e3ab63e07539ff95d71ea850e2
John Castberg <jpcastberg@gmail.com>
2023-08-30 23:49:14 -0700

    Consistent dialog state management

--- client/src/stores/app.ts
+++ client/src/stores/app.ts
@@ -1,16 +1,51 @@
 import { defineStore } from "pinia";
-// import { useGamesStore } from "./games";
+import { useGamesStore } from "./games";
+import { useUserStore } from "./user";
 
 interface AppState {
-    isAppDrawerOpen: boolean;
-    appBarTitle: string;
-    shouldShowInvitePlayerDialog: boolean;
+    shouldShowAppDrawer: boolean;
+    shouldShowRulesDialog: boolean;
+    shouldShowAboutDialog: boolean;
 }
 
 export const useAppStore = defineStore("app", {
     state: (): AppState => ({
-        isAppDrawerOpen: false,
-        appBarTitle: "",
-        shouldShowInvitePlayerDialog: false,
+        shouldShowAppDrawer: false,
+        shouldShowRulesDialog: false,
+        shouldShowAboutDialog: false,
     }),
+    getters: {
+        shouldShowInvitePlayerDialog() {
+            const gamesStore = useGamesStore();
+            const userStore = useUserStore();
+            return (
+                !userStore.userNeedsInitialization &&
+                gamesStore.activeGame?.needToInvitePlayer
+            );
+        },
+        shouldShowNewGameDialog() {
+            const gamesStore = useGamesStore();
+            const userStore = useUserStore();
+            return (
+                !userStore.userNeedsInitialization &&
+                gamesStore.currentGames.size === 0
+            );
+        },
+        shouldShowGameNotFoundDialog() {
+            const userStore = useUserStore();
+            const gamesStore = useGamesStore();
+            return (
+                !userStore.userNeedsInitialization &&
+                gamesStore.activeGameNotFound
+            );
+        },
+        shouldShowNotificationsDialog() {
+            const userStore = useUserStore();
+            return (
+                userStore.user?.sendNotifications === null &&
+                !userStore.needsUsername &&
+                "serviceWorker" in navigator
+            );
+        },
+    },
 });
