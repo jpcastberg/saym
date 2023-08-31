dd9b7676b9debdb723b3b6b3f0369537028ed786
John Castberg <jpcastberg@gmail.com>
2023-08-30 23:46:29 -0700

    Support built app!

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
+import path from "path";
 import express from "express";
-import tokenHandler from "./utils/token_handler";
+import tokenHandler from "./utils/tokenHandler";
 import gamesApi from "./api/games";
 import usersApi from "./api/users";
 
@@ -10,13 +11,10 @@ app.use(express.json());
 app.use(tokenHandler);
 app.use("/api/games", gamesApi);
 app.use("/api/users", usersApi);
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static(path.resolve(__dirname, "../../client/dist")));
 
 app.get("/**", (req, res) => {
-    // intro page, start game, list in-progress
-    // const response = `Your user id is ${res.locals.userId}! Your token is ${res.locals.token}!`;
-    // res.sendFile(__dirname + "/../public/index.html");
-    res.send("response");
+    res.sendFile(path.resolve(__dirname, "../../client/dist/index.html"));
 });
 
 export default app;
