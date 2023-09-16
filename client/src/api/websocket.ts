8e51381dc487d74dd7b123d27192ba290908e224
John Castberg <jpcastberg@gmail.com>
2023-09-16 00:49:21 -0700

    fix websocket bug

--- client/src/api/websocket.ts
+++ client/src/api/websocket.ts
@@ -4,13 +4,36 @@ import {
 } from "../../../shared/models/GameModels";
 
 let websocketCheckInterval: NodeJS.Timer;
-let socket = createWebSocketConnection();
+let websocket = createWebSocketConnection();
 const eventNames = new Set<string>(["gameUpdate"]); // todo: proper enum
 
-socket.addEventListener("message", (event: MessageEvent) => {
+document.addEventListener("visibilitychange", function () {
+    ensureWebsocketConnected();
+});
+
+function createWebSocketConnection() {
+    clearInterval(websocketCheckInterval);
+    websocketCheckInterval = setInterval(ensureWebsocketConnected, 10000);
+    const websocket = new WebSocket(`wss://${location.host}/websocket`);
+    websocket.addEventListener("message", handleIncomingWebsocketMessage);
+    return websocket;
+}
+
+type EventCallback = (event: GameResponseModel) => void;
+type EventListeners = Record<string, EventCallback[]>;
+
+const eventListeners: EventListeners = [...eventNames.values()].reduce(
+    (acc: EventListeners, eventName) => {
+        acc[eventName] = [];
+        return acc;
+    },
+    {},
+);
+
+function handleIncomingWebsocketMessage(event: MessageEvent) {
     const message = event.data as string;
     if (message === "ping") {
-        socket.send("pong");
+        websocket.send("pong");
         return;
     } else if (message === "pong") {
         return;
@@ -25,28 +48,7 @@ socket.addEventListener("message", (event: MessageEvent) => {
             eventListener(parsedEvent.data);
         }
     }
-});
-
-document.addEventListener("visibilitychange", function () {
-    ensureWebsocketConnected();
-});
-
-function createWebSocketConnection() {
-    clearInterval(websocketCheckInterval);
-    websocketCheckInterval = setInterval(ensureWebsocketConnected, 10000);
-    return new WebSocket(`wss://${location.host}/websocket`);
 }
-
-type EventCallback = (event: GameResponseModel) => void;
-type EventListeners = Record<string, EventCallback[]>;
-
-const eventListeners: EventListeners = [...eventNames.values()].reduce(
-    (acc: EventListeners, eventName) => {
-        acc[eventName] = [];
-        return acc;
-    },
-    {},
-);
 
 export { eventNames };
 
@@ -58,16 +60,16 @@ export function listenForEvent(eventName: string, callback: EventCallback) {
 
 export function ensureWebsocketConnected() {
     console.log("socket.readyState:", getSocketReadyState());
-    if (socket.readyState === WebSocket.OPEN) {
-        socket.send("ping");
-    } else if (socket.readyState !== WebSocket.CONNECTING) {
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send("ping");
+    } else if (websocket.readyState !== WebSocket.CONNECTING) {
         console.log("recreating websocket connection");
-        socket = createWebSocketConnection();
+        websocket = createWebSocketConnection();
     }
 }
 
 function getSocketReadyState() {
-    switch (socket.readyState) {
+    switch (websocket.readyState) {
         case WebSocket.OPEN:
             return "OPEN";
         case WebSocket.CONNECTING:
