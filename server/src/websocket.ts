b2c3d494107dacad095116870699fad54c79a367
John Castberg <jpcastberg@gmail.com>
2023-09-16 00:12:45 -0700

    ping pong/clean up old connections

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -1,36 +1,147 @@
-import ws from "ws";
+import ws, { type MessageEvent } from "ws";
 import cookie from "cookie";
 import playersDbApi from "./database/players";
 import tokensDbApi, { type TokenModel } from "./database/token";
 
-const connections = new Map<string, ws>();
+interface PlayerSocket {
+    playerId: string;
+    socket: ws;
+    lastUpdate: Date;
+}
+
+const playerSocketsMap = new Map<string, PlayerSocket[]>();
 const wsServer = new ws.Server({ noServer: true }); // noServer delegates the connection upgrade to express
 
 wsServer.on("connection", async (socket, req) => {
     const providedToken = cookie.parse(req.headers.cookie ?? "").token;
-    const existingToken: TokenModel | null = await tokensDbApi.get(
-        providedToken,
-    );
+    const existingToken: TokenModel | null = await tokensDbApi.get({
+        tokenValue: providedToken,
+    });
 
-    if (existingToken) {
-        const existingPlayer = await playersDbApi.get(existingToken.playerId);
-
-        if (existingPlayer) {
-            connections.set(existingPlayer._id, socket);
-
-            socket.on("close", () => {
-                connections.delete(existingPlayer._id);
-            });
-        }
+    if (!existingToken) {
+        return;
     }
+
+    const existingPlayer = await playersDbApi.get({
+        playerId: existingToken.playerId,
+    });
+
+    if (!existingPlayer) {
+        return;
+    }
+
+    const playerSockets = getOrCreatePlayerSocketsList(existingPlayer._id);
+
+    const newPlayerSocket = {
+        playerId: existingPlayer._id,
+        socket,
+        lastUpdate: new Date(),
+    };
+
+    playerSockets.push(newPlayerSocket);
+
+    initPingPong(newPlayerSocket);
+
+    socket.on("close", () => {
+        console.log("connection closed, removing", existingPlayer._id);
+        removeConnection(existingPlayer._id, newPlayerSocket);
+    });
 });
 
-export function sendWebsocketMessage(playerId: string, message: string) {
-    const recipientConnection: ws | undefined = connections.get(playerId);
+setInterval(cleanupConnections, 60 * 1000);
 
-    if (recipientConnection) {
-        recipientConnection.send(message);
+function sendWebsocketMessage(playerId: string, message: string) {
+    const playerSockets: PlayerSocket[] | undefined =
+        playerSocketsMap.get(playerId);
+
+    if (!playerSockets) {
+        return;
+    }
+
+    for (const playerSocket of playerSockets) {
+        playerSocket.socket.send(message);
     }
 }
 
+function getOrCreatePlayerSocketsList(playerId: string) {
+    let playerSockets = playerSocketsMap.get(playerId);
+
+    if (!playerSockets) {
+        playerSockets = [];
+        playerSocketsMap.set(playerId, playerSockets);
+    }
+
+    return playerSockets;
+}
+
+function initPingPong(playerSocket: PlayerSocket) {
+    const { socket } = playerSocket;
+
+    socket.on("message", (binaryMessage: MessageEvent) => {
+        if (!isActiveSocket(playerSocket)) {
+            console.log(
+                "received message from allegedly inactive socket",
+                playerSocket,
+            );
+        }
+
+        playerSocket.lastUpdate = new Date();
+        const message = String(binaryMessage);
+
+        if (message === "ping") {
+            socket.send("pong");
+        } else if (message === "pong") {
+            setTimeout(() => {
+                socket.send("ping");
+            }, 30000);
+        }
+    });
+
+    socket.send("ping");
+}
+
+function isActiveSocket(playerSocket: PlayerSocket) {
+    const playerSockets = playerSocketsMap.get(playerSocket.playerId);
+    return playerSockets?.includes(playerSocket);
+}
+
+function cleanupConnections() {
+    const currentTime = new Date().getTime();
+    for (const [playerId, playerSockets] of playerSocketsMap.entries()) {
+        const staleConnections: PlayerSocket[] = [];
+        for (const playerSocket of playerSockets) {
+            const inactivityThreshold = 5 * 60 * 1000;
+            const lastUpdatedTime = playerSocket.lastUpdate.getTime();
+
+            if (currentTime - lastUpdatedTime > inactivityThreshold) {
+                staleConnections.push(playerSocket);
+            }
+        }
+
+        for (const staleConnection of staleConnections) {
+            console.log("removing stale connection from player:", playerId);
+            removeConnection(playerId, staleConnection);
+        }
+    }
+}
+
+function removeConnection(playerId: string, playerSocket: PlayerSocket) {
+    const playerSockets = playerSocketsMap.get(playerId);
+    if (playerSockets) {
+        const index = playerSockets.indexOf(playerSocket);
+
+        if (index === -1) {
+            return;
+        }
+
+        playerSocket.socket.terminate();
+        playerSockets.splice(index, 1);
+
+        if (playerSockets.length === 0) {
+            playerSocketsMap.delete(playerId);
+        }
+    }
+}
+
+export { sendWebsocketMessage };
 export default wsServer;
